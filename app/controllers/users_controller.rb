d08c3477fa28c36942e4c179a5aab4da03409a76
Brittany <BrittanyKohler@gmail.com>
2015-11-25 15:52:45 -0800

    Adding views and fixing controller for editing/updating and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,35 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params[:user])
-    # redirect_to "/people/"
+    redirect_to "/"
   end
 
-  def delete
+  def edit
+    id = params[:id]
+    @user = User.find(id)
+    session[:return_to] = request.referer
+  end
+
+  def update
+    id = params[:id]
+    user = User.find(id)
+    user.update(
+    name: user_params[:user][:name],
+    bio: user_params[:user][:bio],
+    color: user_params[:user][:color]
+    )
+    if session[:return_to].nil?
+      redirect_to "/"
+    else
+      redirect_to session[:return_to]
+    end
+    session[:return_to] = nil
+  end
+
+  def destroy
     id = params[:id]
     User.delete(id)
-    # redirect_to "/people"
+    redirect_to "/"
   end
 
   private
