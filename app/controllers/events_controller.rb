05e6b0b6e041d968bef63eea045c67963128ef66
Brittany <BrittanyKohler@gmail.com>
2015-11-25 14:04:10 -0800

    Adding basic CRUD methods to users and events controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,48 @@
 class EventsController < ApplicationController
+  def index
+  @events = event.all
+end
+
+def show
+  id = params[:id]
+  @event = event.find(id)
+end
+
+def new
+  @event = event.new
+end
+
+def create
+  event.create(event_params[:event])
+end
+
+def destroy
+  id = params[:id]
+  event.delete(id)
+  redirect_to "/"
+end
+
+def edit
+  id = params[:id]
+  @event = event.find(id)
+end
+
+def update
+  id = params[:id]
+  event = event.find(id)
+  event.update(
+  name: event_params[:event][:name],
+  description: event_params[:event][:description],
+  event_date: event_params[:event][:event_date],
+  user_id: event_params[:event][:user_id]
+  )
+  redirect_to "/"
+end
+
+
+private
+
+def event_params
+  params.permit(event:[:name, :description, :event_date, :user_id])
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  def index
+    @users = user.all
+  end
+
+  def show
+    id = params[:id]
+    @user = user.find(id)
+  end
+
+  def new
+    @user = user.new
+  end
+
+  def create
+    user.create(user_params[:user])
+    # redirect_to "/people/"
+  end
+
+  def delete
+    id = params[:id]
+    user.delete(id)
+    # redirect_to "/people"
+  end
+
+  private
+
+  def user_params
+    params.permit(user:[:name, :bio, :color])
+  end
 end
