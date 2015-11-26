553b384407abedf93ce323ccb040b1d6c47ed137
Brittany <BrittanyKohler@gmail.com>
2015-11-25 20:49:30 -0800

    Adding gems to support bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'calendar/show'
-
   root 'users#index'
   resources :users do
       resources :events
   end
-
-  resource :calendar, only: [:show], controller: :calendar
 end
