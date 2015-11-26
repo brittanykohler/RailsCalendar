8d1b7d4c21b479d0ccdbfabad74e80dd67e02605
Brittany <BrittanyKohler@gmail.com>
2015-11-26 11:59:34 -0800

    Lots of CSSing on events index page

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,3 +1,5 @@
+# source: https://richonrails.com/articles/building-a-basic-calendar-in-ruby-on-rails
+
 class Calendar < Struct.new(:view, :date, :callback)
     HEADER = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
     START_DAY = :sunday
@@ -5,13 +7,13 @@ class Calendar < Struct.new(:view, :date, :callback)
     delegate :content_tag, to: :view
 
     def table
-      content_tag :table, class: "calendar table table-bordered table-striped" do
+      content_tag :table, class: "table table-bordered" do
         header + week_rows
       end
     end
 
     def header
-      content_tag :tr do
+      content_tag :tr, class: "cal-header" do
         HEADER.map { |day| content_tag :th, day }.join.html_safe
       end
     end
@@ -32,6 +34,7 @@ class Calendar < Struct.new(:view, :date, :callback)
       classes = []
       classes << "today" if day == Date.today
       classes << "not-month" if day.month != date.month
+      classes << "cal-width-height"
       classes.empty? ? nil : classes.join(" ")
     end
 
