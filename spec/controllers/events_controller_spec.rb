5808458517c82ad343507107608cfdda6104d0ec
Ricky Hougland <houglande@gmail.com>
2015-12-24 12:10:06 -0600

    add simplecov gem

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -5,19 +5,15 @@ RSpec.describe EventsController, type: :controller do
     User.create({name: "Taylor", bio: "#SquadGoals"})
   end
 
+  let(:event) do
+    Event.create({name: "Fill that blank space", description: "I guess I'll write your name", event_date: Date.new(2015, 12, 25).to_datetime + Time.parse("17:30").seconds_since_midnight.seconds, user_id: 1})
+  end
+
   describe "GET 'index'" do
     it "gets a success response and renders index template" do
       get :index, user_id: user.id
       expect(response.status).to eq 200
       expect(subject).to render_template :index
     end
-  end
-
-  # describe "GET 'show'" do
-  #   it "gets a success response and renders show template" do
-  #     get :show, user_id: user.id
-  #     expect(response.status).to eq 200
-  #     expect(subject).to render_template :index
-  #   end
   end
 end
