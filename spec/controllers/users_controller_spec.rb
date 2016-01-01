9099fea544c5847b1b40e93f82d51729f62b02a8
emgord <emgord@gmail.com>
2016-01-01 12:05:15 -0800

    added test for new, when logged in can still create a new user so test is failing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -29,7 +29,15 @@ RSpec.describe UsersController, type: :controller do
       end
 
       describe "GET #new" do
-
+        it "responds successfully with an HTTP 200 status code" do
+          get :new
+          expect(response).to be_success
+          expect(response).to have_http_status(200)
+        end
+        it "renders the new template" do
+          get :new
+          expect(subject).to render_template :new
+        end
       end
 
       describe "POST #create" do
@@ -73,7 +81,14 @@ RSpec.describe UsersController, type: :controller do
     end
 
     describe "GET #new" do
-
+      it "is not successful and redirects" do
+        get :new
+        expect(response).to have_http_status(302)
+      end
+      it "redirects to the user show page" do
+        get :new
+        expect(subject).to redirect_to user_path(user)
+      end
     end
 
     describe "POST #create" do
